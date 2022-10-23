8824f7b0d05e0d437cf970b3982d7ad3a312ba67
Russel Porosky <russelporosky@gmail.com>
2022-10-22 23:45:26 -0600

    Auto-generate environment properties from .env file

--- scripts/setenv.ts
+++ scripts/setenv.ts
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
 import ErrnoException = NodeJS.ErrnoException;
 
+const changeCase = require('change-case');
 const { projects } = require('../angular.json');
 const { appendFile, writeFile } = require('fs');
 const { argv } = require('yargs');
@@ -30,18 +31,25 @@ Object.keys(projects)
 		const FileReplacements = Project.architect.build.configurations[CurrentConfiguration]?.fileReplacements || DefaultFileReplacements;
 
 		// read environment variables from .env file
-		require('dotenv').config({ path: `${ EnvironmentsPath }/.env` });
+		const EnvVars = require('dotenv').config({ path: `${ EnvironmentsPath }/.env` });
 
 		// loop through each replacement that matches the `EnvironmentFile` name
 		FileReplacements
 			.filter((fileReplacement: FileReplacement) => fileReplacement.replace.endsWith(EnvironmentFile))
 			.forEach((fileReplacement: FileReplacement) => {
 				// there should only be one iteration, but we don't want to break just in case there isn't
-				const FileContent = `export const environment = {
-	baseUrl: '${ process.env['BASE_URL'] }',
-	production: ${ IsProduction },
-};
-`;
+				const Entries = Object.keys(EnvVars.parsed)
+					.map(varName => ({
+						[changeCase.camelCase(varName)]: EnvVars.parsed[varName],
+					}))
+					.reduce((collector, current) => ({
+						...collector,
+						...current,
+					}), {});
+				const FileContent = `export const environment = ${ JSON.stringify({
+					...Entries,
+					production: IsProduction,
+				}, null, 2) };\n`;
 
 				// write the updated environment file
 				writeFile(`./${ fileReplacement.with }`, FileContent, (err: ErrnoException | null): void => {
